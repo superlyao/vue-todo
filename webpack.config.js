ae1e314556cb1c5f695e9f267ffaf37c18da4b1c
yliao <superyao12@sina.com>
2018-04-17 22:40:43 +0800

    'update'

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path')
+const HTMLplugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const isDev = process.env.NODE_ENV === 'development'
 
-module.exports = {
+const config = {
+  target: "web",
   // 入口
   entry: path.join(__dirname, 'src/index.js'),
   // 出口
@@ -42,5 +46,33 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: isDev ? '"development"' : '"production"'
+      }
+    }),
+    new HTMLplugin()
+  ]
 }
+
+if (isDev) {
+  config.devServer = {
+    port: 8000,
+    host: '0.0.0.0',
+    overlay: {
+      errors: true, //显示错误
+    },
+    hot: true, //只更新组件 不更新整个页面
+    // historyFallback: {
+    //
+    // },
+    open: true, //自动打开浏览器
+  }
+  config.plugins.push(
+    // new webpack.HotModule
+  )
+}
+
+module.exports = config
